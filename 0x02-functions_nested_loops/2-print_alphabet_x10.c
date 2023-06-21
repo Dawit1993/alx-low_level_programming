78235bd650a77284bd23d5bb7059992bec49f8c3
Your Name <you@example.com>
2023-06-20 22:11:59 -0700

    10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - make alphabet x10 times
+ * print_alphabet_x10 - Make alphabet x10 times
  * Return: void
  */
 
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 9 )
+	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
@@ -21,4 +21,5 @@ void print_alphabet_x10(void)
 	_putchar('\n');
 	i++;
 	}
+
 }
