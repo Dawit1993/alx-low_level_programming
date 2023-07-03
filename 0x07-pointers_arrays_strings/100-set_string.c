92d35f7ab9a346a9544495bb14bad183d86084fc
Your Name <you@example.com>
2023-07-03 02:56:01 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - set a value of a pointer to achar
+ * @s: input
+ * @to: input
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
