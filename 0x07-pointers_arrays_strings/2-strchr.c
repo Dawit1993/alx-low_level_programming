7d0ef10843010eb164139f676324846132de3b3e
Your Name <you@example.com>
2023-07-03 19:33:51 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strchr - Entry point
  * @s: input
  * @c: input
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\n'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s + 1);
+		return (s + i);
 	}
 
 	return (NULL);
